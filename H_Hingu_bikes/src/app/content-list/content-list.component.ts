90ed2254fb8ae4dd2abb62b3dfa456e10b434ba9
harshilhingu <91302645+harshilhingu@users.noreply.github.com>
2022-07-20 18:13:39 -0400

    completed with 3 more content

--- H_Hingu_bikes/src/app/content-list/content-list.component.ts
+++ H_Hingu_bikes/src/app/content-list/content-list.component.ts
@@ -50,14 +50,33 @@ export class ContentListComponent implements OnInit {
       hashtags: ["BMW", "Trigger", "Hayabuza"]
     },
     {
-      id: 4,
-      title: 'Hero Honda',
-      body: "Aluminum not that much effective but it's good for some years ",
-      imageLink: "https://static.autox.com/uploads/bikes/2017/05/1486030508-hero-hf-deluxe.jpg",
+      id: 5,
+      title: 'Trigger',
+      body: "it's good for some years ",
+      imageLink: "https://etimg.etb2bimg.com/thumb/msid-48356059,imgsize-83923,width-1200,height-628,overlay-etauto/honda-motorcycle-and-scooter-india-discontinues-cb-trigger-within-30-months-of-launch.jpg",
       type: "sport",
       author: "Harsil Hingu",
       hashtags: ["BMW", "Trigger", "Hayabuza"]
     },
+        {
+      id: 6,
+      title: 'Hayabuza',
+      body: "Aluminum",
+      imageLink: "https://img.etimg.com/thumb/msid-82267745,width-1070,height-580,imgsize-584129,overlay-etpanache/photo.jpg",
+      type: "Racing",
+      author: "Harsil Hingu",
+      hashtags: ["BMW", "Trigger", "Hayabuza"]
+    },    
+    {
+      id: 7,
+      title: 'Ola',
+      body: "Aluminum",
+      imageLink: "https://images.hindustantimes.com/auto/img/2021/01/13/600x338/Royal_Enfiled_Classic_350_1606287446412_1606287452623_1610507147713.jpg",
+      type: "Racing",
+      author: "Harsil Hingu",
+      hashtags: ["BMW", "Trigger", "Hayabuza"]
+    },
+                     
   ];
   }
   ngOnInit(): void {
